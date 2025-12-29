154ee76699e2995bd2f289f3b7039121ecd5ee8f
Amritpal Singh <amritpalsingh@Mac.galaxus.box>
2025-12-30 00:23:35 +0100

    First Commit

---
+++ src/main/java/com/team/onekeysecure/OnekeysecureApplication.java
@@ -0,0 +1,13 @@
+package com.team.onekeysecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnekeysecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnekeysecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/onekeysecure/OnekeysecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.onekeysecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnekeysecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
